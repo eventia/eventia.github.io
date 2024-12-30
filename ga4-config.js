089ec0eb73ed2cef40a68cefff2ff6357666a374
Lim Sungkuk <eventia@gmail.com>
2024-12-30 10:54:43 +0900

    Add files via upload

---
+++ ga4-config.js
@@ -0,0 +1,6 @@
+<script>
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-1RB41TSQTM";
+  document.head.appendChild(script);
+</script>
